6d41dca95bb1461926472f49f307a6305bf4d6d8
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2025-02-07 00:53:45 +0500

    coded

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Footer from "./components/footer";
-import Nav from "./components/TopNav";
+import Nav from "@/pages/components/TopNav";
+import Footer from "@/pages/components/footer";
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
